48e76723c9899b4d51748df185482b8b6570d2cd
2232408653 <2232408653@qq.com>
2020-03-14 13:07:22 +0800

    add msg

--- ds_bug/__manifest__.py
+++ ds_bug/__manifest__.py
@@ -3,15 +3,15 @@
     'name': "ds_bug",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        个人使用学习
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        bug管理系统
+    """,
+
+    'author': "ds",
+    'website': "http://www.odoo12.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
