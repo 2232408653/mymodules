3b4363fa1edb2d290e82693d5aa8e1ebff1bc01a
2232408653 <2232408653@qq.com>
2020-03-17 10:04:07 +0800

    book 7.5try

--- bm_advanced/__manifest__.py
+++ bm_advanced/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/bug_adv.xml',
+        'views/bugs_adv.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
