b82c24f8eee1f93afc72c52572ae8a50e593c79f
2232408653 <2232408653@qq.com>
2020-03-18 10:16:25 +0800

    book 10.1.1

--- bug_wizard/__manifest__.py
+++ bug_wizard/__manifest__.py
@@ -3,15 +3,15 @@
     'name': "bug_wizard",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        bug管理辅助使用模块
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        bug管理辅助使用模块
+    """,
+
+    'author': "DS",
+    'website': "http://odoo12.xyz",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,13 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['ds_bug'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/bug_wizard.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
