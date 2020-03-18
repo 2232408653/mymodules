7a66fa45873e5f73cc734dcec29c8e56f37e0f28
2232408653 <2232408653@qq.com>
2020-03-18 13:25:38 +0800

    book 10.1.4 添加注释

--- bug_wizard/models/bug_wizard.py
+++ bug_wizard/models/bug_wizard.py
@@ -22,11 +22,31 @@ class bugWizard(models.TransientModel):
         defaults=super(bugWizard,self).default_get(fields_names)
         defaults['bug_ids']=self.env.context['active_ids']
         return defaults
+    '''
+    该方法每次处理一个向导实例，所以使用了self.ensure_one()方法。此处的self代表了在向导视图中正在浏览的记录。
+    方法首先验证了在向导窗口中是否有负责人或是否关闭的新值，如果没有则会报错，事实上若两个值都没有的话，
+    那么此批量更新按钮就不会出现。然后用字典存储两个字段的值，使用模型的写入方法一次性写入。
+    方法的最后会返回一个True值，虽然这不是必需的，但读者最好养成这种代码书写习惯。
+    '''
     @api.multi
     def update_batch(self):
         self.ensure_one()
         if not (self.new_is_closed or self.wizard_user_id):
             raise exceptions.ValidationError('无数据要更新')
+        '''
+        使用了_logger，该对象需要在models/bug_wizard.py文件内声明引入，所以需要在类的前面增加如下两行代码：
+        1.    import logging  
+        2.    _logger=logging.getLogger(__name__) 
+        关于logging的使用，需要做一下介绍，为什么要在此处增加日志信息记录呢？因为上例中的批量更新操作，
+        很容易对某些记录进行误选，通过日志记录下来以便于复查。
+        在上面的代码中，_logger是用标准库进行初始化的，
+        而且是在bugWizard类之前。Python内部变量__name__用于标示日志信息来自于本模块。
+        在代码中写下日志消息，可以使用如下四种方法：
+           1.    _logger.debug('DEBUG 信息')  
+           2.    _logger.info('INFO信息')  
+           3.    _logger.warning('WARNING信息')  
+           4.    _logger.error('ERROR信息')   
+        '''
         _logger.debug('批量bug更新操作 %s',self.bug_ids.ids)
         vals={}
         if self.new_is_closed:
