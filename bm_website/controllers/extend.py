9d1e0b97b103fa7786ffdeb39864b089646098fc
2232408653 <2232408653@qq.com>
2020-03-19 19:28:30 +0800

    book 11.1.3 part1

---
+++ bm_website/controllers/extend.py
@@ -0,0 +1,11 @@
+from odoo import http
+#from odoo.addons.bm-website.controllers.main import Main
+#python  不支持减号作为模块的目录名和文件名， 使用如下方法绕过这一限制
+import importlib
+Main = importlib.import_module("odoo.addons.bm_website.controllers.main").Main
+class MainExtended(Main):
+    @http.route()
+    def hello(self, name=None, **kwargs):
+        response = super(MainExtended, self).hello()
+        response.qcontext['name'] = name
+        return response
