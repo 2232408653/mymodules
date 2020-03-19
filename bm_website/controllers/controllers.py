6774cf05979c95ad7f96799df5195bb0f844b3df
2232408653 <2232408653@qq.com>
2020-03-19 17:39:41 +0800

    book 11.1.1 init

---
+++ bm_website/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class BmWebsite(http.Controller):
+#     @http.route('/bm_website/bm_website/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/bm_website/bm_website/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('bm_website.listing', {
+#             'root': '/bm_website/bm_website',
+#             'objects': http.request.env['bm_website.bm_website'].search([]),
+#         })
+
+#     @http.route('/bm_website/bm_website/objects/<model("bm_website.bm_website"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('bm_website.object', {
+#             'object': obj
+#         })
