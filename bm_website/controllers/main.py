8d0f517f334b1933e5dc33ca19db04dcfcae9dc6
2232408653 <2232408653@qq.com>
2020-03-20 16:13:16 +0800

    book 11.2.2 注释1

--- bm_website/controllers/main.py
+++ bm_website/controllers/main.py
@@ -17,6 +17,17 @@ class Main(http.Controller):
         return request.render(
             'bm_website.index',
             {'bugs': bugs})
+    '''
+    控制器检索要使用的数据并使其可用于呈现的模板。
+    在这种情况下，控制器需要经过身份验证的会话，因为该路由具有auth='user'属性。
+    这是默认的行为，显式声明需要用户会话是一种很好的做法。
+    用户登录后，bugs的search()语句将在该上下文中运行并返回相应的记录。
+    '''
+    '''
+    对于不需要经过身份验证访问的控制器，可以读取的数据非常有限。
+    在这种情况下，我们通常需要在上下文中运行部分代码。
+    为此，可以使用sudo()模型方法，它能将安全上下文更改为Admin用户，这样就删除了大多数限制。
+    '''
 
     @http.route('/bug/<model("ds_bug"):bug>',
                 auth="user",  # 默认为user, 但是我们显示指定
